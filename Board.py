2f96b470c73a700ef08273ce1ab5aed71e74bb5a
Aesha Kapoor <aesha.kapoor1@gmail.com>
2022-07-26 14:03:18 -0400

    zombies are 2-4

--- Board.py
+++ Board.py
@@ -433,7 +433,7 @@ class Board:
             else:
                 self.States[x].person = None
         used = []
-        amt_zombies = rd.randint(1, 4)
+        amt_zombies = rd.randint(2, 4)
         for x in range(amt_zombies):
             s = rd.randint(0, len(poss) - 1)
             while s in used:
