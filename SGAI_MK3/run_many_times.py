22a0850f3f2c2053976a420c70fff8ed987c7895
chrehall68 <hallchristopher8697@students.esuhsd.org>
2022-07-30 08:30:44 -0700

    clean up the unneccessary files

--- SGAI_MK3/run_many_times.py
+++
@@ -1,8 +0,0 @@
-import script
-import multiprocessing
-
-if __name__ == "__main__":
-    for i in range(4 * 8):
-        p = multiprocessing.Process(target=script.main)
-        p.start()
-        p.join()
